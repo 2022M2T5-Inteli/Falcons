260ff50f6b008497e4c0b4480845b371c8008122
Mateus Rafael <mateus.silva@sou.inteli.edu.br>
2022-06-10 08:44:09 -0300

    Atualização Eixo 6

--- frontend/formsEixo2/script.js
+++ frontend/formsEixo2/script.js
@@ -1,32 +1,38 @@
-$(document).ready(function () {
+$(document).ready(function(){
     var url = '/Pergunta' //endpoint
     var xhttp = new XMLHttpRequest() //script faz o request para o servidor a partir do URL usando o protocolo http, sem ter q atualizar a pag
     xhttp.open("get", url, false) //define o metódo do request (/get), o endpoint (url), async ou n
     xhttp.send() //envia o request
     var perguntas = JSON.parse(xhttp.responseText) //retorna a resposta em forma de texto (tem q transformar em JSON para poder consultar atributos especificos como .nome; .idade)
     console.log(perguntas)
-    if (perguntas) {
-        url = '/Opcao' //endpoint
-        xhttp = new XMLHttpRequest() //script faz o request para o servidor a partir do URL usando o protocolo http, sem ter q atualizar a pag
-        xhttp.open("get", url, false) //define o metódo do request (/get), o endpoint (url), async ou n
-        xhttp.send() //envia o request
-        var options = JSON.parse(xhttp.responseText) //retorna a resposta em forma de texto (tem q transformar em JSON para poder consultar atributos especificos como .nome; .idade)
-        console.log(options)
-        perguntas = perguntas.filter(pergunta => pergunta.idEixo === 2);
-        if (options) {
-            for (var i = 0; i < perguntas.length; i++) {
-                /*--linha das questões */
-                var caixa = (`<div class="row"> <h4 id=" ${perguntas[i].idPergunta} "> ${perguntas[i].Pergunta} </h4>
+    if (perguntas){
+            url = '/Opcao' //endpoint
+            xhttp = new XMLHttpRequest() //script faz o request para o servidor a partir do URL usando o protocolo http, sem ter q atualizar a pag
+            xhttp.open("get", url, false) //define o metódo do request (/get), o endpoint (url), async ou n
+            xhttp.send() //envia o request
+            var options = JSON.parse(xhttp.responseText) //retorna a resposta em forma de texto (tem q transformar em JSON para poder consultar atributos especificos como .nome; .idade)
+            console.log(options)
+            perguntas = perguntas.filter(pergunta => pergunta.idEixo === 6);
+            if (options){
+                for (var i =0; i < perguntas.length; i++){
+                    /*--linha das questões */
+                    var caixa = (`<div class="row"> <h4 id=" ${perguntas[i].idPergunta} "> ${perguntas[i].Pergunta} </h4>
                     <div style="width: 550px;"> <select class="form-select" aria-label="Default select example">
                         <option selected> Escolha uma opção </option>`)
-                for (var j = 0; j < options.length; j++) {
-                    if (perguntas[i].idTipo == options[j].idTipo) {
-                        caixa += (`<option value="1"> ${options[j].Alternativa} </option>`)
+                    for (var j=0; j<options.length; j++){
+                        if (perguntas[i].idTipo == options[j].idTipo){
+                            caixa += (`<option value="1"> ${options[j].Alternativa} </option>`)
+                        }
                     }
+                    caixa += (`</select> </div> </div>`)
+                    $(font2).append(caixa)
                 }
-                caixa += (`</select> </div> </div>`)
-                $(font2).append(caixa)
             }
-        }
     }
 });
+
+
+
+
+
+
--- frontend/formsEixo6/script.js
+++ frontend/formsEixo6/script.js
@@ -1,23 +1,32 @@
 $(document).ready(function () {
     var url = '/Pergunta' //endpoint
-    var xttp = new XMLHttpRequest() //Script que faz request para o servidor a partir do URL usando o protocolo http, sem ter que atualizar a pagina
-    xhttp.open("get", url, false) //define o metódo do request (/get), o endpoint (url), async ou não
+    var xhttp = new XMLHttpRequest() //script faz o request para o servidor a partir do URL usando o protocolo http, sem ter q atualizar a pag
+    xhttp.open("get", url, false) //define o metódo do request (/get), o endpoint (url), async ou n
     xhttp.send() //envia o request
-    var data = JSON.parse(xhttp.responseText) //retorna a resposta em forma de texto (tem q transformar em JSON para poder consultar atributos especificos como .nome; .idade)
-    console.log(data)
-    document.getElementById("font2").innerHTML = data [58].Pergunta
-    document.getElementById("font3").innerHTML = data [59].Pergunta
-    document.getElementById("font4").innerHTML = data [60].Pergunta
-    document.getElementById("font5").innerHTML = data [61].Pergunta
-    document.getElementById("font6").innerHTML = data [62].Pergunta
-    document.getElementById("font7").innerHTML = data [63].Pergunta
-    document.getElementById("font8").innerHTML = data [64].Pergunta
-    document.getElementById("font9").innerHTML = data [65].Pergunta
-    document.getElementById("font10").innerHTML = data [66].Pergunta
-    document.getElementById("font11").innerHTML = data [67].Pergunta
-    document.getElementById("font12").innerHTML = data [68].Pergunta
-    document.getElementById("font13").innerHTML = data [69].Pergunta
-    document.getElementById("font14").innerHTML = data [70].Pergunta
-    document.getElementById("font15").innerHTML = data [71].Pergunta
-    document.getElementById("font16").innerHTML = data [72].Pergunta
+    var perguntas = JSON.parse(xhttp.responseText) //retorna a resposta em forma de texto (tem q transformar em JSON para poder consultar atributos especificos como .nome; .idade)
+    console.log(perguntas)
+    if (perguntas) {
+        url = '/Opcao' //endpoint
+        xhttp = new XMLHttpRequest() //script faz o request para o servidor a partir do URL usando o protocolo http, sem ter q atualizar a pag
+        xhttp.open("get", url, false) //define o metódo do request (/get), o endpoint (url), async ou n
+        xhttp.send() //envia o request
+        var options = JSON.parse(xhttp.responseText) //retorna a resposta em forma de texto (tem q transformar em JSON para poder consultar atributos especificos como .nome; .idade)
+        console.log(options)
+        perguntas = perguntas.filter(pergunta => pergunta.idEixo === 6);
+        if (options) {
+            for (var i = 0; i < perguntas.length; i++) {
+                /*--linha das questões */
+                var caixa = (`<div class="row"> <h4 id=" ${perguntas[i].idPergunta} "> ${perguntas[i].Pergunta} </h4>
+                    <div style="width: 550px;"> <select class="form-select" aria-label="Default select example">
+                        <option selected> Escolha uma opção </option>`)
+                for (var j = 0; j < options.length; j++) {
+                    if (perguntas[i].idTipo == options[j].idTipo) {
+                        caixa += (`<option value="1"> ${options[j].Alternativa} </option>`)
+                    }
+                }
+                caixa += (`</select> </div> </div>`)
+                $(font2).append(caixa)
+            }
+        }
+    }
 });
